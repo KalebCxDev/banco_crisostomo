e422a3859df781dd1b85ac2bb3cf03360c0ec6c5
KalebCxDev <examplegpt404@gmail.com>
2026-08-31 18:19:16 -0500

    bc_m01: Commit inicial para el proyecto 'banco_crisostomo'

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+class Database {
+    public static function conectar() {
+        // $conexion = new mysqli("localhost", "root", "", "banco_crisostomo");
+        $conexion = new mysqli("localhost", "root", "", "banco_crisostomo", 3308);
+        if ($conexion->connect_error) {
+            die("Error de conexión: " . $conexion->connect_error);
+        }
+        return $conexion;
+    }
+}
+?>
