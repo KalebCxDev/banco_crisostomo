fd402407e44a25e36a431aa176ab3eb144bc84fd
KalebCxDev <examplegpt404@gmail.com>
2026-09-01 21:16:22 -0500

    bc_m02: Modificar 'conexion.php' para adaptarlo a lo requerido

--- config/conexion.php
+++ config/conexion.php
@@ -2,7 +2,7 @@
 class Database {
     public static function conectar() {
         // $conexion = new mysqli("localhost", "root", "", "banco_crisostomo");
-        $conexion = new mysqli("localhost", "root", "", "banco_crisostomo", 3308);
+        $conexion = new mysqli("localhost", "root", "", "banco_crisostomo");
         if ($conexion->connect_error) {
             die("Error de conexión: " . $conexion->connect_error);
         }
