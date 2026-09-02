ed553da4c94df78ddfa5eb0a84b06c79dbba74e4
KalebCxDev <examplegpt404@gmail.com>
2026-09-01 21:37:17 -0500

    Modificar 'index.php'

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'config/database.php';
+require_once 'config/conexion.php';
 require_once 'models/UsuarioModel.php';
 require_once 'controllers/BancoController.php';
 
@@ -14,17 +14,14 @@ switch ($accion) {
     case 'retiro':
         $controlador->retiro();
         break;
- 
     case 'listar':
         $controlador->listarUsuarios();
         break;
     case 'auditoria':
-        $controlador->auditoria();
+        echo "Auditoría no implementada.";
         break;
-        
     default:
-        echo "Bienvenido al Sistema Bancario . <br>";
-        echo "Prueba en la URL con: ?accion=login&u=admin&p=1234";
+        $controlador->inicio();
         break;
 }
 ?>
