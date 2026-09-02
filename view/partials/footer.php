831637ae9242ae695d3d458e76177458438b96a0
KalebCxDev <examplegpt404@gmail.com>
2026-09-01 21:20:01 -0500

    bc_m06: Añadir footer

---
+++ view/partials/footer.php
@@ -0,0 +1,8 @@
+<footer class="bg-light text-center text-lg-start mt-4">
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+        © 2025 Banco ISTO - Todos los derechos reservados
+    </div>
+</footer>
+<script src="https://jsdelivr.net"></script>
+</body>
+</html>
