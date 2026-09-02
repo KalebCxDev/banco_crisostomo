05d54f9efc303e9c558d106801d2cae9c3c2b0c5
KalebCxDev <examplegpt404@gmail.com>
2026-09-01 21:26:00 -0500

    bc_m07: Modificar '/models/UsuarioModel.php', añadir funciones verificaLogin, listarUsuarios

--- models/UsuarioModel.php
+++ models/UsuarioModel.php
@@ -3,17 +3,28 @@ class UsuarioModel {
     private $db;
 
     public function __construct() {
-        $this->db = Database::conectar();
+        $this->db = conexion::conectar();
     }
 
     public function verificarLogin($usuario, $password) {
-        $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND password = '$password'";
+        $sql = "SELECT * FROM usuarios WHERE usuarios = '$usuario' AND password = '$password'";
         $resultado = $this->db->query($sql);
         return $resultado->fetch_assoc();
     }
+
     public function actualizarSaldo($id, $nuevoSaldo) {
         $sql = "UPDATE usuarios SET saldo = $nuevoSaldo WHERE id = $id";
         return $this->db->query($sql);
+    }
+
+    public function listarUsuarios() {
+        $sql = "SELECT id, usuarios, saldos FROM usuarios";
+        $resultado = $this->db->query($sql);
+        $usuarios = [];
+        while ($fila = $resultado->fetch_assoc()) {
+            $usuarios[] = $fila;
+        }
+        return $usuarios;
     }
 }
 ?>
