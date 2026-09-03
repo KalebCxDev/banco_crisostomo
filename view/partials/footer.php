efde5d75390d75dc8822d834be53806f50715cf2
KalebCxDev <examplegpt404@gmail.com>
2026-09-02 20:33:23 -0500

    bc_m14: Cambio de la estructur html de los archivos que conforman el header, nav y foooter

--- view/partials/footer.php
+++ view/partials/footer.php
@@ -1,8 +1,9 @@
 <footer class="bg-light text-center text-lg-start mt-4">
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
         © 2025 Banco ISTO - Todos los derechos reservados
-    </div>
-</footer>
-<script src="https://jsdelivr.net"></script>
+        </div>
+    <footer>
+        <p>© 2025 Banco ISTO - Todos los derechos reservados</p>
+    </footer>
 </body>
 </html>
--- view/partials/nav.php
+++ view/partials/nav.php
@@ -1,16 +1,11 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <div class="container">
-        <a class="navbar-brand" href="index.php">Banco ISTO</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item"><a class="nav-link" href="index.php">Inicio</a></li>
-                <li class="nav-item"><a class="nav-link" href="index.php?accion=login">Login</a></li>
-                <li class="nav-item"><a class="nav-link" href="index.php?accion=retiro">Retiro</a></li>
-                <li class="nav-item"><a class="nav-link" href="index.php?accion=listar">Usuarios</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
+<header>
+    <nav>
+        <a href="index.php" class="logo">Banco <span>ISTO</span></a>
+        <ul class="nav-links">
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="index.php?accion=login">Login</a></li>
+            <li><a href="index.php?accion=retiro">Retiro</a></li>
+            <li><a href="index.php?accion=listar">Usuarios</a></li>
+        </ul>
+    </nav>
+</header>
